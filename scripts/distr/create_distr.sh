56e764aa4b3bc120df19439ff93a4a6f58e1ee1d
Alexey Grebenets <alexey.grebenets@gmail.com>
2016-09-08 13:26:24 +0300

    Обновлен скрипт сборки дистрибутива

--- scripts/distr/create_distr.sh
+++ scripts/distr/create_distr.sh
@@ -2,6 +2,24 @@
 
 ARCHIVE_NAME=server.tar.gz
 
+while [[ $# > 0 ]]
+do
+key="$1"
+
+case $key in
+    -t|--type)
+    TYPE="$2"
+    shift
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift
+done
+
+if [ -z "$TYPE" ]; then echo "You must specify distribution type using --type; distribution can be production or stage"; exit 1; fi
+
 set -e
 set -o xtrace
 
@@ -19,7 +37,20 @@ mkdir -p ${DISTRPATH}
 mkdir -p ${DISTRPATH}/jar
 mkdir -p ${DISTRPATH}/scripts
 
+if [ "$TYPE" == "production" ]
+then
+	cp ${ROOT}/src/main/resources/application-production.properties ${DISTRPATH}/jar/application.properties
+elif [ "$TYPE" == "stage" ]
+then
+	cp ${ROOT}/src/main/resources/application-stage.properties ${DISTRPATH}/jar/application.properties
+else
+	exit 1;
+fi
+
 cp ${ROOT}/build/libs/server.jar ${DISTRPATH}/jar/
 cp ${ROOT}/scripts/*.sh ${DISTRPATH}/scripts/
 
-tar -zcf ${DISTRPATH}/${ARCHIVE_NAME} ${DISTRPATH}
+tar --warning=no-file-changed -zcf ${ARCHIVE_NAME} ${DISTRPATH}
+rm -rf ${DISTRPATH}/*
+mv ${ARCHIVE_NAME} ${DISTRPATH}
+cp ${ROOT}/scripts/distr/install.sh ${DISTRPATH}
